52fa7eaadc97d9c66f0f7eeed0ace5fbf78c9d27
Jose Daniel Az <61296383+jose-az-2001@users.noreply.github.com>
2020-05-05 09:36:36 -0600

    commit extra

--- CifradoCesar/main.cpp
+++ CifradoCesar/main.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-    char nor[26]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
-    char alte[26]={'d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c'};
+    char nor[52]={'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char alte[52]={'d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a','b','c','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C'};
     string frase, frase2;
     cout << "ingrese Frase" << endl;
     getline(cin,frase);
     for(int i = 0; i < frase.length(); i++)
     {
-        for(int x =0; x<27; x++)
+        for(int x =0; x<53; x++)
         {
             if(frase[i]==nor[x])
             {
